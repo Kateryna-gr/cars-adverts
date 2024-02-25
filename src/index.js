39607ae8193a9e2cea50894ab5c6ef48f0ba782c
Kateryna-gr <kategrytsyk@gmail.com>
2024-02-25 03:06:19 +0200

    comment-persistor

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from 'components/GlobalStyles';
-import { store, persistor } from './redux/store';
+import { store /*persistor*/ } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
 import App from 'components/App';
 
